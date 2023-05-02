0cdba1a7f6be1b8d1c03370f0d7837b95fe7abb5
Sergei Gridnev <sergeigr32@gmail.com>
2023-05-02 22:09:20 +0300

    Initial commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,59 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> map = new HashMap<>();
+    List<PageEntry> pageEntry;
+    Map<String, Integer> freqs;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+        for (File item : Objects.requireNonNull(pdfsDir.listFiles())) {
+            PdfDocument doc = new PdfDocument(new PdfReader(item));
+            int pages = doc.getNumberOfPages();
+            for (int i = 1; i <= pages; ++i) {
+                PdfPage pdfPage = doc.getPage(i);
+                int page = doc.getPageNumber(pdfPage);
+                String text = PdfTextExtractor.getTextFromPage(pdfPage);
+                String[] words = text.split("\\P{IsAlphabetic}+");
+                freqs = new HashMap<>();
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+               // System.out.println(freqs);
+                for (Map.Entry<String, Integer> rslts : freqs.entrySet()) {
+                    pageEntry = new ArrayList<>();
+                    if (!map.containsKey(rslts.getKey())) {
+                        PageEntry entry = new PageEntry(item.getName(), page, rslts.getValue());
+                        pageEntry.add(entry);
+                        map.put(rslts.getKey(), pageEntry);
+                    } else {
+                        map.get(rslts.getKey()).add(new PageEntry(item.getName(), page, rslts.getValue()));
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
         // тут реализуйте поиск по слову
+        for (Map.Entry<String, List<PageEntry>> srch : map.entrySet()) {
+            if (srch.getKey().equals(word)) {
+                return srch.getValue();
+            }
+        }
         return Collections.emptyList();
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,29 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+      if(this.count == o.getCount()){
+          return 0;
+      }else if(this.count < o.getCount()){
+          return -1;
+      }else {
+          return 1;
+      }
+    }
+
+    @Override
+    public String toString() {
+        return "Документ: " + pdfName + ", страница: " + page + ", кол-во: " + count;
+    }
 }
